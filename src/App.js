97e8df6036f88b06a06af1126a58d8f412279ee2
maratsr <maratsr@ya.ru>
2021-01-03 21:22:09 +0300

    after 'withRouter()' lesson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 
-function App() {
-  return (
+import './App.css';
+
+const HatsPage = () => (
     <div>
-      <HomePage />
-     </div>
-  );
+        <h1>HATS PAGE </h1>
+    </div>
+);
+
+function App() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/hats' component={HatsPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
