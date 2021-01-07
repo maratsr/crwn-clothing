f4f9e56ea9cf2dca072abe3c0196aa7fe031835f
maratsr <maratsr@ya.ru>
2021-01-07 22:30:50 +0300

    after `Custom Button Component` lesson

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component.jsx';
 
 import './App.css';
 
 function App() {
-    // рендер компонента по URL
+    // рендер компонента по URL + всегда header на экране
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route path='/shop' component={ShopPage} />
+                <Route path='/signin' component={SignInAndSignUpPage} />
             </Switch>
         </div>
     );
